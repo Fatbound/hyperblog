f08d8e8afcbea02303a30686fd767bac9bdad7f8
Jean pierre <piere09@gmail.com>
2022-05-03 17:28:23 -0500

    Esta es la segunda version de mi archivo

--- historia.py
+++ historia.py
@@ -1,1 +1,1 @@
-print("Hola esta es una prueba del repositorio\n vamos a guardarlo    ")
+print("Hola esta es una prueba del repositorio\n vamos a guardarlo  \n tengo 23 años viviendo en todo lado  ")
