7ce3594fe795aad33a4db30839d8cd56362687c1
Jean pierre <piere09@gmail.com>
2022-05-03 18:31:46 -0500

    Cambio de vida

--- historia.py
+++ historia.py
@@ -1,1 +1,1 @@
-print("Hola esta es una prueba del repositorio\n vamos a guardarlo  \n tengo 23 años viviendo en todo lado  ")
+print("Hola esta es una prueba del repositorio\n vamos a guardarlo  \n tengo 23 años viviendo en todo lado \n ahora vamos hablñar de su vida  ")
